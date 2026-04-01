88b4f25ef1ce0e8ae6a7ff42e02b3a0042ab9c1f
stella-quynhthy <stellaqtnguyen@gmail.com>
2026-04-01 19:02:21 -0400

    Updated

---
+++ script.js
@@ -0,0 +1,40 @@
+const cards = [
+    {question: "question1", answer: "answer1"},
+    {question: "question2", answer: "answer2"},
+    {question: "question3", answer: "answer3"}
+];
+
+const prevBtn = document.getElementById("prevBtn");
+const nxtBtn = document.getElementById("nxtBtn");
+const card = document.getElementById("card");
+const cardFront = document.getElementById("front");
+const cardBack = document.getElementById("back");
+let cardIndex = 0;
+
+cardFront.textContent = cards[cardIndex].question;
+
+card.addEventListener("click", () => {
+    cardFront.textContent = "";
+    cardBack.textContent = cards[cardIndex].answer;
+})
+
+nxtBtn.addEventListener("click", () => {
+    if (cardIndex == cards.length-1) {
+        alert("No More Questions Available")
+    } else {
+        cardIndex+=1;
+        console.log(cardIndex)
+        cardBack.textContent = "";
+        cardFront.textContent = cards[cardIndex].question;
+    }
+})
+
+prevBtn.addEventListener("click", () => {
+    if (cardIndex == 0) {
+        alert("You Cannot Go Back Past The First Question")
+    } else {
+        cardIndex-=1;
+        cardBack.textContent = "";
+        cardFront.textContent = cards[cardIndex].question;
+    }
+})
